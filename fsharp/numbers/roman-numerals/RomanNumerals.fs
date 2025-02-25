95427f76449ed69045b12233e25535993b289af8
Matheus Leal <theusleals@outlook.com>
2025-02-25 21:46:58 +0000

    numbers/roman-numerals
    Just cleaning unecessary code.

--- fsharp/numbers/roman-numerals/RomanNumerals.fs
+++ fsharp/numbers/roman-numerals/RomanNumerals.fs
@@ -1,14 +1,5 @@
 module RomanNumerals
 
-type RomanNumeral = 
-    | I = 1
-    | V = 5
-    | X = 10
-    | L = 50
-    | C = 100
-    | D = 500
-    | M = 1000
-
 let roman arabicNumeral = 
 
     let rec convert arabicNumeral = 
